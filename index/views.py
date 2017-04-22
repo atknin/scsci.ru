df544e2194882fb42ff074ac04bfd47d4ea91b5c
atknin <atknini@yahoo.com>
2017-04-23 00:42:16 +0300

    2017-04-22 21:42:16

--- index/views.py
+++ index/views.py
@@ -7,23 +7,9 @@ from django.contrib import auth
 from index import models as index_models
 from index.email_module import sendEmail
 
-def add_to_group(request, pk):
-	arg = {}
-	user = User.objects.get(id=pk)
-	group = Group.objects.get(name='Users')
-	if not user.groups.filter(name = 'Users').exists():
-		group.user_set.add(user)
-		text = user.first_name+', Вы добавлены в базу данных пользователей страницей кооператва "солнечный остров"'
-		link = 'sostrov.com'
-		arg['status'] = user.first_name + ' ' + user.last_name + '('+ user.email+ ')' + 'успешно добавлен в пользователи сайтом.'
-		try:
-			sendEmail(link,text,user.email,'koop.sostrov@gmail.com','soostrov')
-		except:
-			arg['status']+=' Внимание: Пользователь не был уведомлен о его успешном добавлени на сайт, система не email отправить письмо.'
-	else:
-		arg['status'] = 'Пользователь уже добавлен. '
+def about(request):
 	return render(
-	 	request, 'added.html', arg
+	 	request, 'about.html',
 	 	)
 
 def index(request):
