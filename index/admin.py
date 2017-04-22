f826093cd4c46dbdc6909542289989b6d3901eca
atknin <atknini@yahoo.com>
2017-04-23 01:02:38 +0300

    2017-04-22 22:02:38

--- index/admin.py
+++ index/admin.py
@@ -12,11 +12,9 @@ class group_admin(admin.ModelAdmin):
 class doc_admin(admin.ModelAdmin):
 	list_display = (u'name',u'descript')
 
-# class news_admin(admin.ModelAdmin):
-# 	list_display = (u'name', u'descript')
-
+class userAdmin(admin.ModelAdmin):
+	list_display = (u'user', )
 
 admin.site.register(models.group, group_admin)
 admin.site.register(models.doc, doc_admin)
-# admin.site.register(models.news, news_admin)
-
+admin.site.register(models.UserProfile, userAdmin)
