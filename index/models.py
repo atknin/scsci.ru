e83e2537b599bb653a3faa4cd1b6fffbf7d71544
atknin <atknini@yahoo.com>
2017-04-23 00:57:30 +0300

    2017-04-22 21:57:30

--- index/models.py
+++ index/models.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 from django.db import models
 # Create your models here.
+class UserProfile(models.Model):
+	user   = models.OneToOneField(User)
+	avatar = models.ImageField(upload_to='user_avatars/')
+	paper_title = models.CharField(max_length=200,blank = True, null=True)
+	paper_short_description = models.CharField(max_length=500,blank = True, null=True)
+
+	def __unicode__(self):
+		return self.user.username
+
+	def __str__(self):
+		return self.user.username
+	class Meta:
+		verbose_name = 'User'
+		verbose_name_plural = u'Users'
 
 class group(models.Model):
 	name = models.CharField(max_length=30,blank = True, null=True)
@@ -10,12 +24,12 @@ class group(models.Model):
 
 	class Meta:
 		verbose_name = u'Группа'
-		verbose_name_plural = u'Группы' 
+		verbose_name_plural = u'Группы'
 
 	def __unicode__(self):
 		return self.name.encode('utf-8')
 
-	def __str__(self):            
+	def __str__(self):
 		return self.name
 
 class doc(models.Model):
@@ -24,10 +38,10 @@ class doc(models.Model):
 	# picture =  models.ImageField(upload_to='', blank=True)
 	file = models.FileField(upload_to='', blank=True)
 	group = models.ForeignKey('group',on_delete=models.CASCADE,default=1, verbose_name = 'группа')
-	
+
 	class Meta:
 		verbose_name = u'Документ'
-		verbose_name_plural = u'Документы' 
+		verbose_name_plural = u'Документы'
 
 	def __unicode__(self):
 		return self.name.encode('utf-8')
@@ -37,11 +51,10 @@ class doc(models.Model):
 # 	descript = models.CharField(max_length=500, blank = True, null=True)
 # 	picture =  models.ImageField(upload_to='', blank=True)
 # 	file = models.FileField(upload_to='', blank=True)
-	
+
 # 	class Meta:
 # 		verbose_name = u'Новость'
-# 		verbose_name_plural = u'Новости' 
+# 		verbose_name_plural = u'Новости'
 
 # 	def __unicode__(self):
 # 		return self.name.encode('utf-8')
-
