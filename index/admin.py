1327b034837d9205253091ca58ec3578ce667eae
atknin <atknini@yahoo.com>
2017-04-24 17:19:53 +0300

    2017-04-24 14:19:52

--- index/admin.py
+++ index/admin.py
@@ -11,7 +11,7 @@ class userAdmin(admin.ModelAdmin):
 	list_display = (u'user', )
 
 class userfiles(admin.ModelAdmin):
-	list_display = ('name', u'user' )
+	list_display = ('name', u'short_description' )
 
 admin.site.register(models.UserProfile, userAdmin)
 admin.site.register(models.user_files, userfiles)
