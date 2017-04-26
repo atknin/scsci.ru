20b64b479554a34173d8b0f28436f14774a5512f
atknin <atknini@yahoo.com>
2017-04-26 18:01:01 +0300

    2017-04-26 15:01:01

--- index/models.py
+++ index/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
 # Create your models here.
 class add_to_db(models.Model):
 	add_to_db_text = models.CharField(max_length=100)
@@ -15,9 +17,13 @@ class add_to_db(models.Model):
 
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, related_name='UserProfile' unique=True)
+	user = models.OneToOneField(User, related_name='UserProfile', unique=True)
 	avatar = models.ImageField(upload_to='user_avatars/')
-	middle_name = models.CharField(max_length=50,blank = True, null=True)
+	middle_name = models.CharField(max_length=50, blank = True, null=True)
+
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone_number = models.CharField(validators=[phone_regex], blank=True) # validators should be a list
+	
 	paper_title = models.CharField(max_length=200,blank = True, null=True)
 	paper_short_description = models.CharField(max_length=800,blank = True, null=True)
 	birth_day = models.DateField(blank = True, null = True)
