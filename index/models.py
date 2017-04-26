d739dacf4263936c86890a19c94f81c49923d04c
atknin <atknini@yahoo.com>
2017-04-26 18:02:03 +0300

    2017-04-26 15:02:03

--- index/models.py
+++ index/models.py
@@ -22,8 +22,8 @@ class UserProfile(models.Model):
 	middle_name = models.CharField(max_length=50, blank = True, null=True)
 
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], blank=True) # validators should be a list
-	
+	phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True) # validators should be a list
+
 	paper_title = models.CharField(max_length=200,blank = True, null=True)
 	paper_short_description = models.CharField(max_length=800,blank = True, null=True)
 	birth_day = models.DateField(blank = True, null = True)
