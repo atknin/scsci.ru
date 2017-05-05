9015d7b3853981e01ad19083449c8891964d4bc2
atknin <atknini@yahoo.com>
2017-05-05 20:07:45 +0300

    2017-05-05 17:07:45

--- index/updateGalary.py
+++ index/updateGalary.py
@@ -1,8 +1,8 @@
 import vk
 import time
-import requests
 from index import models
-
+from urllib.parse import urlparse
+import os
 def updateGalary():
     token = '311079347:AAFtMNwa4ziqcewP_5cK2d1m3JWulpsmtZg'
     method = 'getUpdates'
@@ -19,14 +19,13 @@ def updateGalary():
             big = a['message']['photo'][-1]['file_id']
             print(small, '\n',big)
             if models.Gallary.objects.filter(photo_id=int(m_id)).exists():
-                print('already exist')
+                pass
             else:
-                print('do not  exist')
                 file_small = requests.get(url='https://api.telegram.org/bot{0}/getFile?file_id={1}'.format(token, small)).json()
                 url_small = 'https://api.telegram.org/file/bot{0}/{1}'.format(token, file_small['result']['file_path'])
                 file_big = requests.get(url='https://api.telegram.org/bot{0}/getFile?file_id={1}'.format(token, big)).json()
                 url_big = 'https://api.telegram.org/file/bot{0}/{1}'.format(token, file_big['result']['file_path'])
-                models.Gallary.objects.create(photo_big=requests.get(url_big).content, photo_small=requests.get(url_small).content, photo_id=int(m_id))
+                models.Gallary.objects.create(photo_big=os.path.basename(urlparse(url_big).path), photo_small=os.path.basename(urlparse(url_small).path), photo_id=int(m_id))
                 print('aded')
         except:
             pass
